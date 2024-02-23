7d2ea1f2b1b2f32e8a857728e4d62c7675a173a3
Franx21 <amvworld2015@gmail.com>
2024-02-23 01:23:29 -0700

    se muestran las vacantes en el dashboard

--- resources/views/livewire/mostrar-vacantes.blade.php
+++ resources/views/livewire/mostrar-vacantes.blade.php
@@ -1,3 +1,30 @@
-<div>
-    {{-- A good traveler has no fixed plans and is not intent upon arriving. --}}
+<div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+    @forelse($vacantes as $vacante)
+    <div class="p-6 bg-white border-b border-gray-200 md:flex md:justify-between md:items-center">
+        <div class="space-y-3">
+            <a href="#" class="text-xl font-bold">
+                {{ $vacante->titulo }}
+            </a>
+            <p class="text-sm text-gray-600 font-bold">{{ $vacante->empresa }}</p>
+            <p class="text-sm text-gray-500">Ultimo dia: {{ $vacante->ultimo_dia }}</p>
+        </div>
+        <div class="flex flex-col items-stretch gap-3 mt-5 md:mt-0">
+            <a href="#" class="bg-slate-800 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center">
+                Candidatos
+            </a>
+            <a href="#" class="bg-blue-800 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center">
+                Editar
+            </a>
+            <a href="#" class="bg-red-800 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center">
+                Eliminar
+            </a>
+        </div>
+
+    </div>
+    @empty
+    <p class="p-3 text-center text-sm text-gray-600">No hay vacantes que mostrar</p>
+    @endforelse
+</div>
+<div class="flex justify-center mt-10">
+    {{ $vacante->links() }}
 </div>
