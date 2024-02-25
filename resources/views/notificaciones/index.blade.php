c77ae2d7ef0d5389fdd2dc01438707494ce920a7
Franx21 <amvworld2015@gmail.com>
2024-02-25 00:05:48 -0700

    se creao el home controler

--- resources/views/notificaciones/index.blade.php
+++ resources/views/notificaciones/index.blade.php
@@ -22,7 +22,8 @@
                             </p>
                         </div>
                         <div class="mt-5 lg:mt-0">
-                            <a href="#" class="bg-indigo-500 p-3 text-sm uppercase font-bold text-white rounded-lg">
+                            <a href="{{ route('candidatos.index', $notificacion->data['id_vacante']) }}"
+                                class="bg-indigo-500 p-3 text-sm uppercase font-bold text-white rounded-lg">
                                 Ver Candidatos
                             </a>
                         </div>
